a4ec5cf7d81a73dad8ae23ee56a0abe61775384a
shintsal <70944299+shintsal@users.noreply.github.com>
2021-03-03 12:05:17 +0200

    add services

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { PropertyCardComponent } from './property/property-card/property-card.component';
 import { PropertyListComponent } from './property/property-list/property-list.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HousingService } from './services/housing.service';
 
 
 @NgModule({
@@ -17,9 +19,10 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [HousingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
