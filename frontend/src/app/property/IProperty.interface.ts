f0c143ee30fa6f1873d6b9b38c60a4f5de7a9f8f
shintsal <70944299+shintsal@users.noreply.github.com>
2021-03-04 08:48:17 +0200

    routing 3

--- frontend/src/app/property/IProperty.interface.ts
+++ frontend/src/app/property/IProperty.interface.ts
@@ -2,7 +2,9 @@
 
 export interface IProperty {
   Id: number;
+  SellRent: number;
   Name: string;
   Type: string,
   Price: number;
+  Image?: string;
 }
--- frontend/src/app/property/property-list/property-list.component.ts
+++ frontend/src/app/property/property-list/property-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { HousingService } from 'src/app/services/housing.service';
 import { IProperty } from '../IProperty.interface';
 
@@ -9,15 +10,21 @@ import { IProperty } from '../IProperty.interface';
 })
 export class PropertyListComponent implements OnInit {
 
+  SellRent = 1;
   properties: Array<IProperty>;
 
-  constructor(private housingService: HousingService) { }
+  constructor(private route: ActivatedRoute,
+              private housingService: HousingService) { }
 
   ngOnInit(): void {
-    this.housingService.getAllProperties().subscribe(
+    if (this.route.snapshot.url.toString()) {
+      this.SellRent = 2; // Means we are on rent-property URL else we are on base URL
+    }
+
+    this.housingService.getAllProperties(this.SellRent).subscribe(
       data => {
         this.properties = data;
-        console.log(data)
+        console.log(data);
       }, error => {
         console.log('httperror:');
         console.log(error);
