d8a64ae0318ccf208db733bfe992d50a81a49099
shintsal <70944299+shintsal@users.noreply.github.com>
2021-03-10 10:03:56 +0200

    create property detail view

--- frontend/src/app/property/property-detail/property-detail.component.ts
+++ frontend/src/app/property/property-detail/property-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { TabsetComponent } from 'ngx-bootstrap/tabs/public_api';
+import { Property } from 'src/app/model/property';
+import { HousingService } from 'src/app/services/housing.service';
 
 @Component({
   selector: 'app-property-detail',
@@ -8,9 +11,11 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class PropertyDetailComponent implements OnInit {
 public propertyId: number;
+property = new Property();
 
   constructor(private route: ActivatedRoute,
-              private router: Router) { }
+              private router: Router,
+              private housingService: HousingService) { }
 
   ngOnInit() {
     this.propertyId = +this.route.snapshot.params['id'];
@@ -18,13 +23,15 @@ public propertyId: number;
     this.route.params.subscribe(
       (params) => {
         this.propertyId = +params['id'];
+        this.housingService.getProperty(this.propertyId).subscribe(
+          (data: Property) => {
+            this.property = data;
+          }
+        )
       }
     );
   }
 
-  onSelectNext() {
-    this.propertyId += 1;
-    this.router.navigate(['property-detail', this.propertyId]);
-  }
+
 
 }
--- frontend/src/app/services/housing.service.ts
+++ frontend/src/app/services/housing.service.ts
@@ -14,7 +14,15 @@ export class HousingService {
 
   constructor(private http: HttpClient) { }
 
-  getAllProperties(SellRent: number): Observable<IPropertyBase[]> {
+  getProperty(id: number) {
+    return this.getAllProperties().pipe(
+      map(propertiesArray => {
+        return propertiesArray.find(p => p.Id === id);
+      })
+    );
+  }
+
+  getAllProperties(SellRent?: number): Observable<IPropertyBase[]> {
     return this.http.get('data/properties.json').pipe(
       map(data => {
         const propertiesArray: Array<IPropertyBase> = [];
@@ -22,16 +30,24 @@ export class HousingService {
 
         if (localProperties) {
           for (const id in localProperties) {
+            if (SellRent) {
             if (localProperties.hasOwnProperty(id) && localProperties[id].SellRent === SellRent) {
+              propertiesArray.push(localProperties[id]);
+            }
+            } else {
               propertiesArray.push(localProperties[id]);
             }
           }
         }
 
         for (const id in data) {
+          if (SellRent) {
           if (data.hasOwnProperty(id) && data[id].SellRent === SellRent) {
           propertiesArray.push(data[id]);
           }
+        } else {
+          propertiesArray.push(data[id]);
+        }
         }
         return propertiesArray;
       })
