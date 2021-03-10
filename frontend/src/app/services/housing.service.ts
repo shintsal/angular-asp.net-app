089a68473647e7135e7b8deb3b0c2ad4a157b1ef
shintsal <70944299+shintsal@users.noreply.github.com>
2021-03-10 07:58:44 +0200

    saving multiple properties

--- frontend/src/app/services/housing.service.ts
+++ frontend/src/app/services/housing.service.ts
@@ -18,6 +18,15 @@ export class HousingService {
     return this.http.get('data/properties.json').pipe(
       map(data => {
         const propertiesArray: Array<IPropertyBase> = [];
+        const localProperties = JSON.parse(localStorage.getItem('newProp'));
+
+        if (localProperties) {
+          for (const id in localProperties) {
+            if (localProperties.hasOwnProperty(id) && localProperties[id].SellRent === SellRent) {
+              propertiesArray.push(localProperties[id]);
+            }
+          }
+        }
 
         for (const id in data) {
           if (data.hasOwnProperty(id) && data[id].SellRent === SellRent) {
@@ -31,7 +40,24 @@ export class HousingService {
   }
 
   addProperty(property: Property) {
-    localStorage.setItem('newProp', JSON.stringify(property));
+    let newProp = [property];
+
+    // Add new property in array if newProp already exists in local storage
+    if (localStorage.getItem('newProp')) {
+      newProp = [property,
+                  ...JSON.parse(localStorage.getItem('newProp'))];
+    }
+
+    localStorage.setItem('newProp', JSON.stringify(newProp));
+  }
+
+  newpPropID() {
+    if (localStorage.getItem('PID')) {
+      return +localStorage.getItem('PID') + 1;
+    } else {
+      localStorage.setItem('PID', '101');
+      return 101;
+    }
   }
 
 }
